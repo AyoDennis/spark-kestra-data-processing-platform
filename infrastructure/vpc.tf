0e1cf74b35d3fe57b965dad4110d3827e14ecf60
AyoDennis <ayodejidennis33@gmail.com>
2025-04-20 17:22:29 +0100

    public subnet 2

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -22,3 +22,7 @@ resource "aws_subnet" "public-subnet-1" {
   }
 }
 
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.emr-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  
