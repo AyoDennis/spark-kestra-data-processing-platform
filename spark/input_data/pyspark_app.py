7164f687ce2f0fcdade1ec61e684be7721594c6e
AyoDennis <ayodejidennis33@gmail.com>
2025-06-04 10:05:40 +0100

    update pyspark

--- spark/input_data/pyspark_app.py
+++ spark/input_data/pyspark_app.py
@@ -1,8 +1,4 @@
 
-
-
-
-
 # import pyspark
 # from pyspark.sql import SparkSession
 # from pyspark.sql.functions import *
@@ -34,29 +30,29 @@
 
 
 
-# import pyspark
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import *
-# from pyspark.sql.types import *
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
-# # Initialize Spark with proper logging configuration
-# spark = SparkSession.builder \
-#     .appName('Aggregation on Single col') \
-#     .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:/etc/spark/log4j.properties") \
-#     .getOrCreate()
+# Initialize Spark with proper logging configuration
+spark = SparkSession.builder \
+    .appName('Aggregation on Single col') \
+    .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:/etc/spark/log4j.properties") \
+    .getOrCreate()
 
-# # Reduce log noise (optional))
-# spark.sparkContext.setLogLevel("WARN")
+# Reduce log noise (optional))
+spark.sparkContext.setLogLevel("WARN")
 
-# print("Hello0000000000")
-# df = spark.read.csv('s3a://spark-job-data-input-test/data-source/emp_dataset.csv', header=True, inferSchema=True)
+print("Hello0000000000")
+df = spark.read.csv('s3a://spark-job-data-input-test/data-source/emp_dataset.csv', header=True, inferSchema=True)
 
-# # df = spark.read.csv('s3a://ayodeji-data-ingestion/random_profile/males/91d0d032d3154e669916870e33bb6dd7.snappy.parquet', header=True, inferSchema=True)
+# df = spark.read.csv('s3a://ayodeji-data-ingestion/random_profile/males/91d0d032d3154e669916870e33bb6dd7.snappy.parquet', header=True, inferSchema=True)
 
-# print("I have accessed the dataset now")
+print("I have accessed the dataset now")
 
-# df.groupBy("BusinessTravel").agg({"Age": "sum"}).sort("BusinessTravel").show()
-# df = df.groupBy("BusinessTravel").agg({"Age": "sum"}).sort("BusinessTravel")
-# rename_df = df.withColumnRenamed('sum(Age)', 'total_age')
-# rename_df.show()
-# rename_df.write.parquet("s3a://spark-job-data-output/spark_output_test/employee/",mode="overwrite")
+df.groupBy("BusinessTravel").agg({"Age": "sum"}).sort("BusinessTravel").show()
+df = df.groupBy("BusinessTravel").agg({"Age": "sum"}).sort("BusinessTravel")
+rename_df = df.withColumnRenamed('sum(Age)', 'total_age')
+rename_df.show()
+rename_df.write.parquet("s3a://spark-job-data-output/spark_output_test/employee/",mode="overwrite")
